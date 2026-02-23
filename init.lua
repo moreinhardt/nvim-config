1d7da6d0f7dcb24dd0558a1f9dc0baab9b3ad967
Moritz Reinhardt <moreinhardt@gmail.com>
2026-02-23 15:38:36 +0100

    feat(telescope): add mappings to show available keys

--- init.lua
+++ init.lua
@@ -397,11 +397,17 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          mappings = {
+            i = {
+              ['<C-enter>'] = 'to_fuzzy_refine',
+              ['<C-h>'] = 'which_key',
+            },
+            n = {
+              ['?'] = 'which_key',
+            },
+          },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = { require('telescope.themes').get_dropdown() },
