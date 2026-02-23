9da7da9cd9329518e4b2b4fa4c5d070979f0b814
Moritz Reinhardt <moreinhardt@gmail.com>
2026-02-23 15:37:41 +0100

    feat: add autoclose plugin to automatically insert closing brackets

--- init.lua
+++ init.lua
@@ -271,6 +271,9 @@ require('lazy').setup({
   -- NOTE: Plugins can be added via a link or github org/name. To run setup automatically, use `opts = {}`
   { 'NMAC427/guess-indent.nvim', opts = {} },
 
+  -- Automatically insert closing brackets
+  { 'm4xshen/autoclose.nvim', opts = {} },
+
   -- Alternatively, use `config = function() ... end` for full control over the configuration.
   -- If you prefer to call `setup` explicitly, use:
   --    {
