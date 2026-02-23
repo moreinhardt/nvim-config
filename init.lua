faa9a55ca7d6fb1707b880cddb434b58109b3bca
Moritz Reinhardt <moreinhardt@gmail.com>
2026-02-23 15:55:57 +0100

    feat(conform): add autoformat for python

--- init.lua
+++ init.lua
@@ -750,8 +750,15 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+
+        python = function(bufnr)
+          if require('conform').get_formatter_info('ruff_format', bufnr).available then
+            return { 'ruff_format' }
+          else
+            return { 'isort', 'black' } -- runs them sequentially
+          end
+        end,
+
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
